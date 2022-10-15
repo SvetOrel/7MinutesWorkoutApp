4b2c0dc26f3ace4d9c5e182fb3d3b3fc79283295
sunli <sunlightsvett@gmail.com>
2022-10-15 19:16:38 +0300

    7 Minute Workout

--- app/src/main/java/com/example/a7minutesworkout/FinishActivity.kt
+++ app/src/main/java/com/example/a7minutesworkout/FinishActivity.kt
@@ -2,7 +2,13 @@ package com.example.a7minutesworkout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.lifecycle.lifecycleScope
 import com.example.a7minutesworkout.databinding.ActivityFinishBinding
+import kotlinx.coroutines.launch
+import java.text.SimpleDateFormat
+import java.util.*
 
 class FinishActivity : AppCompatActivity() {
     private var binding: ActivityFinishBinding? = null
@@ -23,6 +29,26 @@ class FinishActivity : AppCompatActivity() {
 
         binding?.btnFinish?.setOnClickListener{
             finish()
+        }
+
+        val historyDao = (application as WorkOutApp).db.historyDao()
+        addDateToDatabase(historyDao)
+    }
+
+    private fun addDateToDatabase(historyDao: HistoryDao){
+        val myCalendar = Calendar.getInstance()
+        val dateTime = myCalendar.time
+        Log.e("Date: ",""+dateTime)
+        val sdf = SimpleDateFormat("dd MMM yyyy HH:mm:ss",Locale.getDefault())
+        val date = sdf.format(dateTime)
+        Log.e("Formatted Date: ",""+date)
+        if(date.isNotEmpty()){
+            lifecycleScope.launch {
+                historyDao.insert(HistoryEntity(date))
+                Toast.makeText(applicationContext,"Record Saved",Toast.LENGTH_LONG)
+            }
+        }else{
+            Toast.makeText(applicationContext,"Date cannot be blank",Toast.LENGTH_LONG)
         }
     }
 }
--- app/src/main/java/com/example/a7minutesworkout/HistoryActivity.kt
+++ app/src/main/java/com/example/a7minutesworkout/HistoryActivity.kt
@@ -2,7 +2,11 @@ package com.example.a7minutesworkout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.lifecycleScope
 import com.example.a7minutesworkout.databinding.ActivityHistoryBinding
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 class HistoryActivity : AppCompatActivity() {
 
@@ -21,5 +25,23 @@ class HistoryActivity : AppCompatActivity() {
         binding?.toolbarHistoryActivity?.setNavigationOnClickListener {
             onBackPressed()
         }
+
+        val dao = (application as WorkOutApp).db.historyDao()
+        getAllCompletedDates(dao)
+    }
+
+    private fun getAllCompletedDates(historyDao: HistoryDao){
+        lifecycleScope.launch{
+            historyDao.fetchAllDates().collect{ allCompletedDatesList ->
+                for(i in allCompletedDatesList){
+                    Log.e("Date: ",""+i)
+                }
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
     }
 }
